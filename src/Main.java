b099603a6c891cf53065ff351ab49619494670fb
Prasang41 <harshitchaudhary568@gmail.com>
2026-05-02 09:52:19 +0530

    Greeting

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+//        Name and Birth of the Bot
         BirthOfBot birthOfBot = new BirthOfBot("Aid", 2026);
 
         System.out.println("Hello! My name is "+ birthOfBot.getBotName());
         System.out.println("I was created in "+ birthOfBot.getBirthOfBot());
+
+//        Introduction and Greeting the user
+        System.out.println("Please, Remind me your name.");
+        System.out.print(" > ");
+        UserIntroduction userIntroduction = new UserIntroduction(scanner.next());
+        System.out.println("What a great name you have, "+ userIntroduction.getYourName());
     }
 }
---
+++ src/UserIntroduction.java
@@ -0,0 +1,11 @@
+public class UserIntroduction {
+    private String yourName;
+
+    public UserIntroduction(String yourName){
+        this.yourName = yourName;
+    }
+
+    public String getYourName() {
+        return this.yourName;
+    }
+}
